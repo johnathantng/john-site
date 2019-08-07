21dd6f117cab798ee4dc89887c871b43b8185588
johnathantng <johnathan.tng@gmail.com>
2019-08-06 20:31:03 -0700

    navbar-mobile now working - need css updates

--- src/components/NavBarMobile.js
+++ src/components/NavBarMobile.js
@@ -1,8 +1,53 @@
 import { useState, useEffect } from 'react';
 
-const NavBarMobile = () => {
+import Menu from '../../static/icons/menu.png';
+import Logo from '../../static/logos/jn.png';
+
+const NavBarMobile = (props) => {
+	const [isNavOpen, setNavOpen] = useState(false);
+	const [slide, setSlide] = useState('slide-out')
+
+	const handleSlide = () => {
+		if (slide == "slide-in") {
+		  setSlide("slide-out");
+		} else {
+			setSlide("slide-in");
+		};
+	};
+
+	const	toggleNavOpen = () => {
+
+		setNavOpen(true);
+
+		handleSlide();
+
+	};
+
+	const handleNavBar = () => {
+		if (isNavOpen) {
+			return (
+				<div className={`nav-mobile-container ${slide}`}>
+					<div className="nav-mobile-item">
+						<span>work</span>
+					</div>
+					<div className="nav-mobile-item">
+						<span>about me</span>
+					</div>
+				</div>
+			);
+		};
+	};
+
 	return (
-		<div> Mobile </div>
+		<div className="navbar-mobile">
+			<div className="mobile-header">
+				<img className="nav-mobile-logo" alt="jn-logo" src={Logo} />
+				<img className="nav-mobile-menu" alt="menu" src={Menu} onClick={toggleNavOpen} />
+			</div>
+			<div>
+				{handleNavBar()}
+			</div>
+		</div>
 	);
 };
 
