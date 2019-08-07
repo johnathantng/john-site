a97d98223ab795fcbd4348e80f5a814f4f89cac6
johnathantng <johnathan.tng@gmail.com>
2019-08-06 19:16:44 -0700

    updated packages and sass files - created NavBar example

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import Link from 'next/link'
 
+import NavBar from '../src/components/NavBar';
+
+import '../sass/index.scss';
+
 const Home = () => (
-  <div> Hello World! </div>
+	<div>
+		<NavBar />
+	</div>
 )
 
 export default Home;
