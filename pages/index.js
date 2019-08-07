cbd4f1b333015cec1ef04d0cb1979d7c9271ff1a
johnathantng <johnathan.tng@gmail.com>
2019-08-06 21:19:57 -0700

    save 08/06/2019

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Link from 'next/link'
 
 import NavBar from '../src/components/NavBar';
+import Hero from '../src/Hero';
 
 import '../sass/index.scss';
 
 const Home = () => (
 	<div>
 		<NavBar />
+		<Hero />
 	</div>
 )
 
---
+++ src/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import heroBackground from '../static/images/jojobgtransparent.png';
+
+const Hero = () => {
+	return (
+		<div className="hero-container">
+			Hero
+		</div>
+	);
+};
+
+export default Hero;
